0d4bffee828937053f8b90dada765c458b502f84
sonkarjiaditya-dot <sonkarjiaditya@gmail.com>
2026-09-13 14:15:39 +0530

    Update books from Admin Panel

--- src/data/books.js
+++ src/data/books.js
@@ -244,7 +244,7 @@ export const books = [
     "language": "English",
     "isFree": true,
     "description": "Learn about money management ",
-    "published": false,
+    "published": true,
     "pdfUrl": "https://res.cloudinary.com/xy6is0nn/image/upload/v1789288974/odm81tbwwcd29xhg9j0q.pdf"
   }
 ];
